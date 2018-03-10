fe5a66b554ffd6e919ff84b6c6b9cfa6c6b4889e
Tardis Xu <xiaoxubeii@gmail.com>
2018-03-10 12:07:32 +0800

    Init the cloud interface of Kubernetes cloud provider.

--- qcloud/qcloud.go
+++ qcloud/qcloud.go
@@ -1,1 +1,55 @@
 package qcloud
+
+// CMV is a wrapper of QCloud CVM.
+type CVM struct {
+}
+
+// CLB is a wrapper of QCloud CVM.
+type CLB struct {
+}
+
+// QCloud is an implementation of cloud provider Interface for Tencent Cloud.
+type QCloud struct {
+	cvm    CVM
+	clb    CLB
+	region string
+}
+
+// Initialize provides the cloud with a kubernetes client builder and may spawn goroutines
+// to perform housekeeping activities within the cloud provider.
+func (c *QCloud) Initialize(clientBuilder controller.ControllerClientBuilder) {}
+
+// LoadBalancer returns a balancer interface. Also returns true if the interface is supported, false otherwise.
+func (c *QCloud) LoadBalancer() (LoadBalancer, bool) {
+	return c, true
+}
+
+// Instances returns an instances interface. Also returns true if the interface is supported, false otherwise.
+func (c *QCloud) Instances() (Instances, bool) {
+	return c, true
+}
+
+// Zones returns a zones interface. Also returns true if the interface is supported, false otherwise.
+func (c *QCloud) Zones() (Zones, bool) {
+	return c, true
+}
+
+// Clusters returns a clusters interface.  Also returns true if the interface is supported, false otherwise.
+func (c *QCloud) Clusters() (Clusters, bool) {
+	return c, true
+}
+
+// Routes returns a routes interface along with whether the interface is supported.
+func (c *QCloud) Routes() (Routes, bool) {
+	return c, true
+}
+
+// ProviderName returns the cloud provider ID.
+func (c *QCloud) ProviderName() string {
+	return "qcloud"
+}
+
+// HasClusterID returns true if a ClusterID is required and set
+func (c *QCloud) HasClusterID() bool {
+	return true
+}
